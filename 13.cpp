a0ebc621034b75dee1c529b1bf5a211b5d255e69
LoL070302 <47444525+LoL070302@users.noreply.github.com>
2019-02-15 15:37:43 +0900

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int a, b, c, d, i, sum = 0;
+	char aa, bb, cc, dd;
+	//문재 73
+	//do
+	//{
+	//	scanf("%d", &a);
+	//	printf("%d\n", a);
+	//}while(a);
+	
+	
+	//문재 78
+	//scanf("%d",&a);
+	//for(i = 1; i <= a; i++)
+	//{
+	//	if(i % 2 == 0)
+	//	{
+	//		sum += i;
+	//	}
+	//}
+	//printf("%d\n", sum);
+	
+	//문재 79
+	//do
+	//{
+	//	scanf("%c", &aa);
+	//	printf("%c", aa);
+	//}while(aa != 'q');
+	
+	//문재 82 ???
+	//문재 87
+	//scanf("%d", &a);
+	//for(i = 1;;i++)
+	//{
+	//	sum += i;
+	//	if(sum >= a)
+	//	{
+	//		printf("%d\n", sum);
+	//		return 0;
+	//	}
+	//}
+	//문재 91, 변수 이름:a = a, m = b, d = c, n = d
+	//scanf("%d %d %d %d", &a, &b, &c, &d);
+	//sum = 1;
+	//for(i = 1; i < d; i++)
+	//{
+	//	sum = sum*b+c; 	
+	//} 
+	//printf("%d\n", sum);
+	
+	
+}
